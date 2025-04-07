f267a6e339939b7ce0b518ad57ce6c0f43bdb2e8
greenmay9 <maydaymm66@yahoo.com>
2025-04-07 12:11:05 -0600

    cambios en setup.py en packages se agregaron corchetes

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ author_emails = ", ".join(author["email"] for author in AUTHORS)
 setup (
     name = 'smv-prueba1',  #para el import
     version ='0.0.1', #se va cambiando según se incluyan cambios
-    packages= 'smv_prueba1', #Debe coincidir con el nombre de la carpeta
-    description= 'libreia para sumar dos numeros',
-    long_description= open('README.md', encoding='utf-8').read(),
-    long_description_content_type= "text/markdown",
-    author=author_names,
-    author_email=author_emails,
-    url='https://github.com/greenmay99',
-    install_requires=['pandas', 'numpy', 'polars',
+    packages = ['smv_prueba1'], #Debe coincidir con el nombre de la carpeta
+    description = 'libreia para sumar dos numeros',
+    long_description = open('README.md', encoding='utf-8').read(),
+    long_description_content_type = "text/markdown",
+    author = author_names,
+    author_email = author_emails,
+    url = 'https://github.com/greenmay99',
+    install_requires = ['pandas', 'numpy', 'polars',
                       'seaborn', 'scipy', 'matplotlib'],
-    license='MIT' #Tipo de licencia
+    license = 'MIT' #Tipo de licencia
 )
