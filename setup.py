446b77f995fe07d71fbe57a40f7043668cd6f925
greenmay9 <maydaymm66@yahoo.com>
2025-04-07 13:25:11 -0600

    actualizacion del setup

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ author_names = ", ".join(author["name"] for author in AUTHORS)
 author_emails = ", ".join(author["email"] for author in AUTHORS)
 
 setup (
-    name = 'smv-prueba1',  #para el import
+    name = 'smv_prueba1',  #para el import
     version ='0.0.2', #se va cambiando según se incluyan cambios
-    packages = ['smv_prueba1'], #Debe coincidir con el nombre de la carpeta
+    #packages = ['smv_prueba1'], #Debe coincidir con el nombre de la carpeta
     description = 'libreia para sumar dos numeros',
     long_description = open('README.md', encoding='utf-8').read(),
     long_description_content_type = "text/markdown",
