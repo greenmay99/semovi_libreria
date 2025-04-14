92b777ea8a955cdca344c87802118c38021d91b4
greenmay9 <maydaymm66@yahoo.com>
2025-04-13 18:57:51 -0600

    actualizacion del setup y estructura de carpetas para pypi

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
-from setuptools import setup
+import setuptools
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+#información de contacto de los autores
 AUTHORS = [ 
-    { "name": "may", "email": "maydaymm66@yahoo.com" }, 
-    { "name": "juan", "email": "juan@example.com" }
+    { "name": "@greenmay", "email": "maydaymm66@yahoo.com" }, 
+    { "name": "@juan", "email": "juan@example.com" }
 ]
-
-# Convertir lista de autores a string
 author_names = ", ".join(author["name"] for author in AUTHORS)
 author_emails = ", ".join(author["email"] for author in AUTHORS)
 
-setup (
-    name = 'smv_prueba1',  #para el import
-    version ='0.0.2', #se va cambiando según se incluyan cambios
-    #packages = ['smv_prueba1'], #Debe coincidir con el nombre de la carpeta
-    description = 'libreia para sumar dos numeros',
-    long_description = open('README.md', encoding='utf-8').read(),
+setuptools.setup (
+    name = 'SMVLibreria',  #para el import
+    version ='1.0.0', #se va cambiando según se incluyan cambios
+    description = 'Libreria para sumar dos números',
+    long_description = long_description,
     long_description_content_type = "text/markdown",
     author = author_names,
     author_email = author_emails,
     url = 'https://github.com/greenmay99/semovi_libreria',
-    license = 'MIT' #Tipo de licencia
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10"
 )
--- src/SMVLibreria/__init__.py
+++ src/SMVLibreria/__init__.py
@@ -3,4 +3,3 @@
 def suma(num1, num2):
     resultado = num1 + num2
     return resultado
-
